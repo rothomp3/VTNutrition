c24eb6554267341955872df86e6d016949a19a24
Robert Thompson <rothomp3@vt.edu>
2013-04-10 22:42:05 -0400

    Added template code for Core Data stack

--- VTNutrition/VTNAppDelegate.h
+++ VTNutrition/VTNAppDelegate.h
@@ -14,4 +14,11 @@
 
 @property (strong, nonatomic) UITabBarController *tabBarController;
 
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
 @end
